1cc07126cc0c8bc6147935aaeae5f11b6f29e4be
Mahiuha <tiasho.mahiuha@gmail.com>
2021-09-09 20:46:31 +0300

    imports the function def add(a, b): and print the result

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,69 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * _realloc - Reallocates a memory block
+ * @ptr: The pointer to the previous memory block
+ * @old_size: The size of the old memory block
+ * @new_size: The size of the new memory block
+ *
+ * Return: The pointer to the new memory block otherwise NULL
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *new_ptr;
+	unsigned int min_size = old_size < new_size ? old_size : new_size;
+	unsigned int i;
+
+	if (new_size == old_size)
+		return (ptr);
+	if (ptr != NULL)
+	{
+		if (new_size == 0)
+		{
+			free(ptr);
+			return (NULL);
+		}
+		new_ptr = malloc(new_size);
+		if (new_ptr != NULL)
+		{
+			for (i = 0; i < min_size; i++)
+				*((char *)new_ptr + i) = *((char *)ptr + i);
+			free(ptr);
+			return (new_ptr);
+		}
+		free(ptr);
+		return (NULL);
+	}
+	else
+	{
+		new_ptr = malloc(new_size);
+		return (new_ptr);
+	}
+}
+
+
+
+/**
+ * check_cycle - checks if a singly linked list has a cycle in it
+ * @list: list head
+ * Return: 0 if no, 1 if yes
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *fast, *slow = list;
+
+	if (list == NULL)
+		return (0);
+
+	fast = list->next;
+	while (slow != NULL && fast != NULL && fast->next != NULL)
+	{
+		if (slow == fast)
+			return (1);
+		fast = fast->next->next;
+		slow = slow->next;
+	}
+	return (0);
+}

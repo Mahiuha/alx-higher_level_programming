0424b87dc95854087ed8a35b70ad8bf733666558
Mahiuha <tiasho.mahiuha@gmail.com>
2021-10-04 23:21:59 +0300

    spacing

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -8,7 +8,6 @@ def all_possible(n=4):
     """ Function to find all possible solutions by placing the first queen on
         the first row, with different column positions starting from 2nd
         column to 2nd to last column.
-
         Args:
             n (int): The size of the chess board, but also the number of queens
                 to place on the board.
@@ -30,7 +29,6 @@ def recur_bt(matrix, row, col, n):
         have been placed in non threatening positions) or all possible
         combinations of `row` and `col` with the current matrix yields no
         solution.
-
         Args:
             matrix (:obj:`list` of :obj:`list` or int): A matrix list of lists
                 representing the positions of queens placed on the board.
@@ -40,7 +38,6 @@ def recur_bt(matrix, row, col, n):
                 queens can be placed on.
             n (int): The size of the board, but also the number of queens that
                 can fit.
-
         Returns:
             A list of lists which is a solution, or None if the current members
             of the matrix lead to a dead end.
@@ -69,7 +66,6 @@ def recur_bt(matrix, row, col, n):
 def bot_right(matrix, y, x, n):
     """
         Function to test if there are any queens on the bottom right diagonal.
-
         Args:
             matrix (:obj:`list` of :obj:`list` or int): A matrix list of lists
                 representing the positions of queens placed on the board.
@@ -77,7 +73,6 @@ def bot_right(matrix, y, x, n):
                 queens can be placed on.
             x (int): "column" or x co-ordinate.
             n (int): The size of the board.
-
         Returns:
             True if no queens exist on the bottom right diagonal otherwise
             False.
@@ -94,7 +89,6 @@ def bot_right(matrix, y, x, n):
 def bot_left(matrix, y, x, n):
     """
         Function to test if there are any queens on the bottom left diagonal.
-
         Args:
             matrix (:obj:`list` of :obj:`list` or int): A matrix list of lists
                 representing the positions of queens placed on the board.
@@ -102,7 +96,6 @@ def bot_left(matrix, y, x, n):
                 queens can be placed on.
             x (int): "column" or x co-ordinate.
             n (int): The size of the board.
-
         Returns:
             True if no queens exist on the bottom left diagonal otherwise
             False.
@@ -119,7 +112,6 @@ def bot_left(matrix, y, x, n):
 def top_left(matrix, y, x, n):
     """
         Function to test if there are any queens on the top left diagonal.
-
         Args:
             matrix (:obj:`list` of :obj:`list` or int): A matrix list of lists
                 representing the positions of queens placed on the board.
@@ -127,7 +119,6 @@ def top_left(matrix, y, x, n):
                 queens can be placed on.
             x (int): "column" or x co-ordinate.
             n (int): The size of the board.
-
         Returns:
             True if no queens exist on the top left diagonal otherwise False.
     """
@@ -143,7 +134,6 @@ def top_left(matrix, y, x, n):
 def top_right(matrix, y, x, n):
     """
         Function to test if there are any queens on the top right diagonal.
-
         Args:
             matrix (:obj:`list` of :obj:`list` or int): A matrix list of lists
                 representing the positions of queens placed on the board.
@@ -151,7 +141,6 @@ def top_right(matrix, y, x, n):
                 queens can be placed on.
             x (int): "column" or x co-ordinate.
             n (int): The size of the board.
-
         Returns:
             True if no queens exist on the top right diagonal otherwise False.
     """

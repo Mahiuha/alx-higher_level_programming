093bafb720a7ccaafaaa23aa88c3d091875a0df2
Mahiuha <tiasho.mahiuha@gmail.com>
2021-11-17 23:08:46 +0300

    creates the MySQL server user user_0d_1.

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- Create a root user
+-- Create a root user on localhost
+CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO user_0d_1@localhost;

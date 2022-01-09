4888d9f3ee255a47ba53fec25857197707602695
Mahiuha <tiasho.mahiuha@gmail.com>
2022-01-09 16:24:42 +0300

    a bash script that sends a POST request to a url

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # a Bash script that takes in a URL, sends a POST request to the passed URL, and displays the body of the response.
-curl -sd "email=test@gmail.com & subject=I will always be here for PLD" "$1"
+curl -s "$1" -X POST -d "email=test@gmail.com&subject=I will always be here for PLD"

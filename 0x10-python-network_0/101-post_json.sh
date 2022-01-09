7264128bb9e626e21c33af90252826abd8360f82
Mahiuha <tiasho.mahiuha@gmail.com>
2022-01-09 17:08:12 +0300

    a script that sends a POST request

---
+++ 0x10-python-network_0/101-post_json.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# a Bash script that makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing You got me!, in the body of the response.
+curl -sX POST -H "Content-Type: application/json" -b @"$2" "$1"

2d66aa224f266b26a866c3264ed228592fbe9b5e
Mahiuha <tiasho.mahiuha@gmail.com>
2022-01-09 17:58:34 +0300

    gets the status of intranet.hbtn.io/status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,13 +5,10 @@
 import urllib.request
 
 if __name__ == "__main__":
-    try:
-        with urllib.request.urlopen('https://intranet.hbtn.io/status') as resp:
-            if resp is not None:
-                html = resp.read()
-                print("Body response:")
-                print("\t- type: {}".format(type(html)))
-                print("\t- content: {}".format(html))
-                print("\t- utf8 content: {}".format(html.decode('utf-8')))
-    except Exception:
-        pass
+
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        html = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(html))
+        print("\t- utf8 content: {}".format(html.decode()))

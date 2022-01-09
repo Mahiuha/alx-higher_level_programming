8758eb4958a7728e42577e8ad0902201b39dc5ce
Mahiuha <tiasho.mahiuha@gmail.com>
2022-01-09 18:26:27 +0300

    script that fetches https://intranet.hbtn.io/status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,10 +5,13 @@
 import urllib.request
 
 if __name__ == "__main__":
-
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-        html = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(html)))
-        print("\t- content: {}".format(html))
-        print("\t- utf8 content: {}".format(html.decode()))
+    try:
+        with urllib.request.urlopen('https://intranet.hbtn.io/status') as resp:
+            if resp is not None:
+                html = resp.read()
+                print("Body response:")
+                print("\t- type: {}".format(type(html)))
+                print("\t- content: {}".format(html))
+                print("\t- utf8 content: {}".format(html.decode('utf-8')))
+    except Exception:
+        pass

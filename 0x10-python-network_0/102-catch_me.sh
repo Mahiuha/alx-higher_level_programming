d5b1d3816a0ed48f1a249b2bb4b8ad60d6150a3b
Mahiuha <tiasho.mahiuha@gmail.com>
2022-01-09 17:23:18 +0300

    curl until You got me!

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #  a Bash script that makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing You got me!, in the body of the response.
-curl -sL -X PUT 0.0.0.0:5000/catch_me -d "user_id=98" -H "Origin: School"
+curl -sL -X PUT 0.0.0.0:5000/catch_me -d "user_id=98" -H "Origin: HolbertonSchool"

90fe022dd2bd2c0c7e7d998b04eb12dbbdffc3e0
Mahiuha <tiasho.mahiuha@gmail.com>
2022-01-09 17:32:21 +0300

    binary search

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,20 +1,24 @@
 #!/usr/bin/python3
 """ Finds Peak values """
-def find_peak(list_of_integers):
-    """Find the peak"""
-    list_l = len(list_of_integers)
-    if list_l is 0:
-        return None
-    peak = binary_search(list_of_integers, 0, list_l - 1)
-    return list_of_integers[peak]
+def find_peak2(numbers, size, start, end):
+    middle = int(start + (end - start) / 2)
+
+    if ((middle is 0 or numbers[middle - 1] <= numbers[middle]) and
+       (middle == size - 1 or numbers[middle + 1] <= numbers[middle])):
+        return numbers[middle]
+    elif (middle > 0 and numbers[middle - 1] > numbers[middle]):
+        return find_peak2(numbers, size, start, (middle - 1))
+    else:
+        return find_peak2(numbers, size, middle + 1, end)
 
 """ binary search algorithim """
-def binary_search(a, lo, hi):
-    """Recursive binary search of the peak"""
-    if lo >= hi:
-        return lo
-    mid = ((hi - lo) // 2) + lo
-    if a[mid] > a[mid + 1]:
-        return binary_search(a, lo, mid)
-    else:
-        return binary_search(a, mid + 1, hi)
+def find_peak(list_of_integers):
+    if len(list_of_integers) == 0:
+        return None
+    if len(list_of_integers) == 1:
+        return list_of_integers[0]
+    if len(list_of_integers) == 2:
+        return (list_of_integers[0] if list_of_integers[0] >
+                list_of_integers[1] else list_of_integers[1])
+    return (find_peak2(list_of_integers, len(list_of_integers), 0,
+            len(list_of_integers) - 1))

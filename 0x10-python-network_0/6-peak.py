2fb80dab670a05003134d3ac940ce5ea30bee034
Mahiuha <tiasho.mahiuha@gmail.com>
2022-01-10 19:38:02 +0300

    making 6-peak.py PEP8 compliant

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """ Finds Peak values """
+
+
 def find_peak(list_of_integers):
     """Find the peak"""
     list_l = len(list_of_integers)
@@ -7,6 +9,7 @@ def find_peak(list_of_integers):
         return None
     peak = binary_search(list_of_integers, 0, list_l - 1)
     return list_of_integers[peak]
+
 
 """ binary search algorithim """
 def binary_search(a, lo, hi):
